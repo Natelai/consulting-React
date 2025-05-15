a744be201e21288b8217db032c7d10388889d877
Daria Kyselytsia <daria.kys1@gmail.com>
2025-05-15 20:43:19 +0300

    add pictures

--- src/components/DreyfusTest/index.jsx
+++ src/components/DreyfusTest/index.jsx
@@ -87,15 +87,45 @@ const DreyfusTestPage = () => {
     setOverallScore(0);
   };
 
+  const levelImageMap = {
+    'Новачок': 'Beginner.png',
+    'Твердий початківець': 'AdvancedBeginner.png',
+    'Компетентний': 'Competent.png',
+    'Досвідчений': 'Proficient.png',
+    'Експерт': 'Expert.png'
+  };
+
+
   if (showResults) {
     const percentage = (overallScore / 80) * 100;
+    const level = getLevelDescription(overallScore);
+    const levelTitle = level.title;
+    const levelDescription = level.description;
+
     return (
       <div className="results-page">
-        <h2>Ваш результат: {overallScore}</h2>
-        <p>{getLevelDescription(overallScore)}</p>
-        <p>Ваш рівень вищий, ніж у {percentage.toFixed(2)}% шукачів роботи.</p>
-        <Chart answers={chartData} />
-        <button onClick={restartTest} className="restart-button">Пройти знову</button>
+        <div className="results-container">
+          <div className="results-image">
+            <img
+              src={require(`./${levelImageMap[levelTitle]}`)}
+              alt={levelTitle}
+            />
+          </div>
+          <div className="results-content">
+            <h2>{levelTitle}</h2>
+            <p>Ваш рівень вищий, ніж у {percentage.toFixed(2)}% шукачів роботи.</p>
+            <p className="level-description">{levelDescription}</p>
+            <div className="chart-container">
+              <Chart answers={chartData} />
+            </div>
+
+            <button onClick={restartTest} className="restart-button">Пройти знову</button>
+          </div>
+
+
+        </div>
+
+
       </div>
     );
   }
@@ -141,11 +171,11 @@ const DreyfusTestPage = () => {
       ))}
 
       <div className="navigation-buttons">
-        <button onClick={handlePrevious} disabled={currentBlockIndex === 0}>Previous</button>
+        <button style={{ backgroundColor: 'purple' }} onClick={handlePrevious} disabled={currentBlockIndex === 0}>Назад</button>
         {currentBlockIndex === blocksOrder.length - 1 ? (
-          <button onClick={handleFinish}>Finish</button>
+          <button style={{ backgroundColor: 'greenyellow' }} onClick={handleFinish}>Завершити</button>
         ) : (
-          <button onClick={handleNext}>Next</button>
+          <button style={{ backgroundColor: 'green' }} onClick={handleNext}>Далі</button>
         )}
       </div>
     </div>
@@ -153,11 +183,40 @@ const DreyfusTestPage = () => {
 };
 
 const getLevelDescription = (score) => {
-  if (score <= 16) return "Ви новачок.";
-  if (score <= 32) return "Ви початківець.";
-  if (score <= 48) return "Ви компетентний.";
-  if (score <= 64) return "Ви просунутий.";
-  return "Ви експерт.";
+  if (score <= 16) {
+    return {
+      title: "Новачок",
+      description:
+        "На цьому етапі кандидат має обмежений досвід і не здатен бачити контекстуальні особливості."
+    };
+  }
+  if (score <= 32) {
+    return {
+      title: "Твердий початківець",
+      description:
+        "Особа на цьому етапі починає помічати контекстуальні особливості, але все ще покладається на чіткі правила та інструкції."
+    };
+  }
+  if (score <= 48) {
+    return {
+      title: "Компетентний",
+      description:
+        "Особа здатна робити більш складні рішення, оскільки має досвід роботи з різними ситуаціями."
+    };
+  }
+  if (score <= 64) {
+    return {
+      title: "Досвідчений",
+      description:
+        "Кандидат починає бачити загальну картину і здатний прогнозувати можливі результати своїх рішень."
+    };
+  }
+  return {
+    title: "Експерт",
+    description:
+      "Людина на цьому етапі володіє глибоким розумінням своєї сфери, працює без чітких інструкцій і може надавати рекомендації навіть у складних ситуаціях."
+  };
 };
+
 
 export default DreyfusTestPage;
